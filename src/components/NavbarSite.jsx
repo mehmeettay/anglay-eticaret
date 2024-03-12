5435ebd5c5976ebf1f853e64dfb8fb6b53ab26cc
mehmeettay <memookayy@gmail.com>
2024-03-12 12:58:06 +0300

    yeni_navbar

--- src/components/NavbarSite.jsx
+++ src/components/NavbarSite.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
@@ -12,64 +13,102 @@ import { FaSearch } from "react-icons/fa";
 
 import "./Navbar.css";
 import { FormControl } from "react-bootstrap";
+
 const NavbarSite = () => {
+  const handleToggle = () => {
+    setIsOpen(!isOpen); // isOpen durumunu tersine çevirir
+};
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <Navbar expand="lg" className="navbar-main deneme">
+      <Navbar
+        expand="lg"
+        className="navbar-main deneme  pl-5"
+      >
         <Container fluid>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
-            <div className="d-flex flex-column w-100">
+            <div className="d-flex flex-column w-100 pt-1 ">
               <div className=" w-auto  d-flex flex-wrap align-items-center justify-content-between">
-                <div className="">
+                <div className="cursor-pointer">
                   <img className="logo" src={logo} alt="" />
                 </div>
                 <div className="">
                   <Form className="d-flex gap-3">
-                 
                     <Form.Control
                       type="search"
-                      placeholder="Search"
+                      placeholder="Arama Yapınız"
                       className="search"
                       aria-label="Search"
-                   
                     />
-                    
-                    <Button variant="outline-success bg-light">Search</Button>
+
+                    <Button className="w-25" variant="light">
+                      Arama Yap
+                    </Button>
                   </Form>
                 </div>
                 <div className="">
                   <div className="d-flex gap-3 ">
                     <div className="icon1 ">
-                      <FaUser  />
+                      <FaUser />
                     </div>
                     <div className="icon2">
-                    <IoMdCart className="cart" />
+                      <IoMdCart className="cart" />
                     </div>
-                    
                   </div>
                 </div>
               </div>
-              <div >
+              <div>
                 <Nav
-                  className="me-auto my-2 my-lg-0 "
-                  style={{ maxHeight: "200px", height: "100px" }}
+                  className="me-auto mt-lg-3 "
+                  style={{
+                    maxHeight: "200px",
+                    height: "auto",
+                    
+                  }}
                   navbarScroll
                 >
-                  
-                  <NavDropdown title="Tüm Kategoriler" id="navbarScrollingDropdown">
-                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action4">
-                      Another action
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action5">
-                      Something else here
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                  <Nav.Link href="#action1">En çok Satanlar</Nav.Link>
-                  <Nav.Link href="#action2">Fırsat Ürünleri</Nav.Link>
-                 
+                  <div className="d-flex gap-4 ">
+                    <div>
+                    <NavDropdown
+                        title="Tüm Kategoriler"
+                        id="basic-nav-dropdown"
+                        show={isOpen}
+                        onMouseEnter={handleToggle}
+                        onMouseLeave={handleToggle}
+                        className= {isOpen ? "nav-dropdown active" : "nav-dropdown"}
+                    >
+                      
+                        <NavDropdown.Item href="#action3">
+                          Bilgisayar
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="#action4">
+                          Telefon
+                        </NavDropdown.Item>
+                        
+                        <NavDropdown.Item href="#action5">
+                          TV
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="#action5">
+                          Beyaz Eşya
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="#action5">
+                          Ev Aletleri
+                        </NavDropdown.Item>
+                      </NavDropdown>
+                    </div>
+                    <div>
+                    <button type="button" class="btn btn-outline-light mh-25px">En çok Satanlar</button>
+                    </div>
+                    <div>
+                      
+                      <button type="button" class="btn btn-outline-light">Fırsat Ürünleri</button>
+                    </div>
+                    <div>
+                    <button type="button" class="btn btn-outline-light">Kampanyalar</button>
+                    </div>
+                    
+                  </div>
                 </Nav>
               </div>
             </div>
