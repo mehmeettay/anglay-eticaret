a0be3fb6a24b3720a062f8191efc8efcd85be47a
mehmeettay <memookayy@gmail.com>
2024-03-13 10:58:09 +0300

    navbar_update

--- src/components/NavbarSite.jsx
+++ src/components/NavbarSite.jsx
@@ -9,22 +9,19 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import logo from "../assets/img/anglay-logo-white.png";
 import { FaUser } from "react-icons/fa";
 import { IoMdCart } from "react-icons/io";
-import { FaSearch } from "react-icons/fa";
-
+import { IoSearch } from "react-icons/io5";
 import "./Navbar.css";
-import { FormControl } from "react-bootstrap";
+import { IoMdNotificationsOutline } from "react-icons/io";
 
 const NavbarSite = () => {
   const handleToggle = () => {
     setIsOpen(!isOpen); // isOpen durumunu tersine çevirir
-};
+  };
   const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
-      <Navbar
-        expand="lg"
-        className="navbar-main deneme  pl-5"
-      >
+      <Navbar expand="lg" className="navbar-main deneme  pl-5">
         <Container fluid>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
@@ -33,24 +30,22 @@ const NavbarSite = () => {
                 <div className="cursor-pointer">
                   <img className="logo" src={logo} alt="" />
                 </div>
-                <div className="">
-                  <Form className="d-flex gap-3">
-                    <Form.Control
-                      type="search"
-                      placeholder="Arama Yapınız"
-                      className="search"
-                      aria-label="Search"
-                    />
 
-                    <Button className="w-25" variant="light">
-                      Arama Yap
-                    </Button>
-                  </Form>
+                <div className="search-bar d-flex justify-content-between">
+                  <input
+                    type="text"
+                    placeholder="Aramak istediğiniz kelimeyi giriniz..."
+                  />
+                  <IoSearch className="search-icon" />
                 </div>
+
                 <div className="">
                   <div className="d-flex gap-3 ">
+                  <div className="icon1 ">
+                  <IoMdNotificationsOutline className="notification" />
+                    </div>
                     <div className="icon1 ">
-                      <FaUser />
+                      <FaUser  className="user"/>
                     </div>
                     <div className="icon2">
                       <IoMdCart className="cart" />
@@ -58,37 +53,35 @@ const NavbarSite = () => {
                   </div>
                 </div>
               </div>
-              <div>
+              <div className="mb-2">
                 <Nav
                   className="me-auto mt-lg-3 "
                   style={{
                     maxHeight: "200px",
                     height: "auto",
-                    
                   }}
                   navbarScroll
                 >
                   <div className="d-flex gap-4 ">
                     <div>
-                    <NavDropdown
+                      <NavDropdown
                         title="Tüm Kategoriler"
                         id="basic-nav-dropdown"
                         show={isOpen}
                         onMouseEnter={handleToggle}
                         onMouseLeave={handleToggle}
-                        className= {isOpen ? "nav-dropdown active" : "nav-dropdown"}
-                    >
-                      
+                        className={
+                          isOpen ? "nav-dropdown active" : "nav-dropdown"
+                        }
+                      >
                         <NavDropdown.Item href="#action3">
                           Bilgisayar
                         </NavDropdown.Item>
                         <NavDropdown.Item href="#action4">
                           Telefon
                         </NavDropdown.Item>
-                        
-                        <NavDropdown.Item href="#action5">
-                          TV
-                        </NavDropdown.Item>
+
+                        <NavDropdown.Item href="#action5">TV</NavDropdown.Item>
                         <NavDropdown.Item href="#action5">
                           Beyaz Eşya
                         </NavDropdown.Item>
@@ -98,16 +91,23 @@ const NavbarSite = () => {
                       </NavDropdown>
                     </div>
                     <div>
-                    <button type="button" class="btn btn-outline-light mh-25px">En çok Satanlar</button>
+                      <button
+                        type="button"
+                        className="btn btn-outline-light mh-25px"
+                      >
+                        En çok Satanlar
+                      </button>
                     </div>
                     <div>
-                      
-                      <button type="button" class="btn btn-outline-light">Fırsat Ürünleri</button>
+                      <button type="button" className="btn btn-outline-light">
+                        Fırsat Ürünleri
+                      </button>
                     </div>
                     <div>
-                    <button type="button" class="btn btn-outline-light">Kampanyalar</button>
+                      <button type="button" className="btn btn-outline-light">
+                        Kampanyalar
+                      </button>
                     </div>
-                    
                   </div>
                 </Nav>
               </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Slider from '../pages/Slider'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    Home
+    </>
   )
 }
 
